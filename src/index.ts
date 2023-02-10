c01edac399847104b328c7143eb26b4b83b6bd4a
Samuel Pires <samuel.ferpires@gmail.com>
2023-02-10 20:43:34 -0300

    Adding express-session and Session types to express

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,29 @@
 import Express from "express";
 import cors from "cors";
 import apiRoutes from "./router";
+import expressSession from "express-session";
+import { PrismaSessionStore } from "@quixo3/prisma-session-store";
+import { PrismaClient } from "@prisma/client";
 
 const setupServer = async () => {
     const app = Express();
+
+    app.use(
+        expressSession({
+            name: "webquest-auth",
+            cookie: {
+                maxAge: 7 * 24 * 60 * 60 * 1000, // ms
+            },
+            secret: "a santa at nasa",
+            resave: true,
+            saveUninitialized: true,
+            store: new PrismaSessionStore(new PrismaClient(), {
+                checkPeriod: 2 * 60 * 1000, //ms
+                dbRecordIdIsSessionId: true,
+                dbRecordIdFunction: undefined,
+            }),
+        })
+    );
 
     app.use(cors());
 
