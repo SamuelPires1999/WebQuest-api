b8811fbba4707c6adb0a5fa67c9a35887b0f33f7
Samuel Pires <samuel.ferpires@gmail.com>
2023-02-10 14:21:34 -0300

    Express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Nodejs AIO boilerplate 2.0");
+import Express from "express";
+import cors from "cors";
+const setupServer = async () => {
+    const app = Express();
+
+    app.use(cors());
+
+    app.listen(4000, () => {
+        console.log("Application running");
+    });
+};
+
+setupServer().catch(error => {
+    console.log(error);
+});
